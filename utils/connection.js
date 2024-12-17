3d99760f1d9bb9681e1388d0275d1f0ac748ef28
rohnshrma <rohn11shrma@gmail.com>
2024-12-17 16:43:35 +0530

    env changes

--- utils/connection.js
+++ utils/connection.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export default async function connectDB() {
   try {
-    const conn = await mongoose.connect(
-      `mongodb+srv://admin-rohan11sharma:${process.env.MONGODB_PASSWORD}@cluster0.ogyw5zf.mongodb.net/wgfsb1blogAppDB`
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to db | host :", conn.connection.host);
   } catch (err) {
     console.log(err);
