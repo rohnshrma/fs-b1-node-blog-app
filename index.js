ba0a6ffb689ea51b69a317377abf3e7d0b16cea1
rohnshrma <rohn11shrma@gmail.com>
2025-01-07 12:40:19 +0530

    add user registration and login functionality with bcrypt for password hashing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose, { mongo } from "mongoose";
 import bodyParser from "body-parser";
 import { configDotenv } from "dotenv";
 import connectDB from "./utils/connection.js";
+import bcrypt from "bcryptjs";
 
 configDotenv();
 const app = express();
@@ -11,6 +12,7 @@ const PORT = 3000;
 connectDB();
 
 // schema
+// blog schema
 const blogSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, minLength: 20 },
@@ -21,8 +23,18 @@ const blogSchema = new mongoose.Schema(
   }
 );
 
+// userschema
+const userschema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minLength: 8 },
+  },
+  { timestamps: true }
+);
+
 // model | collection
-const Blog = mongoose.model("blog", blogSchema);
+const Blog = mongoose.model("Blog", blogSchema);
+const User = mongoose.model("User", userschema);
 
 // bodyparser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,6 +44,64 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 // ROUTES
+
+// register
+app
+  .route("/register")
+  .get(function (req, res) {
+    res.render("Register", {
+      title: "Register",
+      year: new Date().getFullYear(),
+    });
+  })
+  .post(async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+      var salt = await bcrypt.genSalt(10);
+      var hash = await bcrypt.hash(password, salt);
+
+      const newuser = new User({
+        username: username,
+        password: hash,
+      });
+
+      await newuser.save();
+
+      console.log(newuser);
+      res.redirect("/compose");
+    } catch (err) {
+      return res.status(400).json({ error: err });
+    }
+  });
+
+// LOGIN ROUTE
+app
+  .route("/login")
+  .get(function (req, res) {
+    res.render("Login", {
+      title: "Login",
+      year: new Date().getFullYear(),
+    });
+  })
+  .post(async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+      const existingUser = await User.findOne({ username: username });
+      if (!existingUser) {
+        return res.status(404).json({ error: "User Not Found" });
+      }
+      var result = await bcrypt.compare(password, existingUser.password);
+      if (!result) {
+        return res.status(400).json({ error: "Incorrect Password" });
+      }
+      res.redirect("/compose");
+    } catch (err) {
+      return res.status(400).json({ error: err });
+    }
+  });
+
 // HOME : ROOT : "/"
 app.route("/").get(function (req, res) {
   res.render("Home", {
